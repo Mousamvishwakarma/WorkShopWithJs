497f1b8a3244f057c29714b95fe9b59a086a6b05
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-10-12 23:38:05 +0530

    create a todo app for  way to data stored

--- 11-10-2025/todoapp.js
+++ 11-10-2025/todoapp.js
@@ -1,11 +1,12 @@
-const { createElement } = require("react")
+
+
 
 
 let todo = []
 let progress = []
-// let done = []
+let done = []
 
-const rendertodo = () =>{
+const renderTodo = () =>{
     const todolist = document.getElementById('todo')
 todolist.innerHTML = `<h3>Todo</h3>`
 for(let val in todo){
@@ -20,18 +21,77 @@ div.innerHTML`<p> ${todo[val]}</p>
   const renderProgress = ()=>{
     const Progresslist = document.getElementById('Progress')
     Progresslist.innerHTML =` <h3>Progress</h3>`
-    for(let val in Progress){
+    for(let val in progress){
       const div = createElement('div')
       div.className = 'task'
-      div.innerHTML =`<p> ${Progress[val]}
+      div.innerHTML =`<p> ${progress[val]}
         <button onclick='moveToProgress(${el})'> > </button>
        `
        Progresslist.appendChild(div)
     }
     
   }
+  const renderDone = () =>{
+    const doneList = document.getElementById('done')
+    doneList.innerHTML = `<h3>Done</h3>`
+    for(let val in done){
+      const div = createElement(div)
+         div.className="task"
+        div.innerHTML= `
+        <p> ${done[el]}</p>
+        `
+        doneList.appendChild(div)
 
-   
-  const addTask = () =>{
-    rendertodo()
+    }
   }
+   
+const addTask = () => {
+    event.preventDefault()
+    const task = event.target[0].value
+    todo.push(task)
+    localStorage.setItem('todo', JSON.stringify(todo))
+    event.target[0].value = ''
+    console.log(todo)
+    renderTodo()
+}
+
+const moveToProgress = (id) => {
+    console.log("moved to progress", id)
+    progress.push(todo[id])
+    todo.splice(id,1)
+    localStorage.setItem('todo', JSON.stringify(todo))
+    localStorage.setItem('progress', JSON.stringify(progress))
+    renderTodo()
+    renderProgress()
+    
+} 
+
+const moveToDone = (id) => {
+    done.push(progress[id])
+    progress.splice(id,1)
+    
+    renderProgress()
+    renderDone()
+    localStorage.setItem('progress', JSON.stringify(progress))
+    localStorage.setItem('done', JSON.stringify(done))
+}
+
+
+const init = () => {
+    console.log('init')
+    todo = JSON.parse(localStorage.getItem('todo'))
+    progress = JSON.parse(localStorage.getItem('progress'))
+    done = JSON.parse(localStorage.getItem('done'))
+    if(todo?.length){
+        renderTodo()
+    }
+    if(progress?.length){
+        renderProgress()
+    }if(done?.length){
+        renderDone()
+    }
+}
+
+window.onload = () =>{
+    init()
+}
