a076f6db317e35ef47d4523bce9014f243232718
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-10-13 20:01:09 +0530

    call back function

---
+++ 13-10-2025/callback.js
@@ -0,0 +1,15 @@
+// // setTimeout =()=>{
+// // console.log("hello");
+
+// // }
+// // console.log(hello );
+
+//  function add (a,b){
+//     setTimeout(()=>{
+// console.log(a+b);
+
+//     },0)
+//  }
+//  add(10,20)
+  
+
